077582c557e2e9b9357b272d4e5158b3541d8bdb
80978448 <80978448@corp.root>
2016-11-03 17:29:42 +0800

    starwizard start

---
+++ android/src/com/jon/starwizard/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jon.starwizard;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jon.starwizard.StarWizardGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarWizardGame(), config);
+	}
+}
